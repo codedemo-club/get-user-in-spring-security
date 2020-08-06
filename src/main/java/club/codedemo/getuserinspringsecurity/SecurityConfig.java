c3275f306c51b5eb8846c56f2d3e9887a3a9b095
panjie <3792535@qq.com>
2020-08-06 15:12:04 +0800

    init

---
+++ src/main/java/club/codedemo/getuserinspringsecurity/SecurityConfig.java
@@ -0,0 +1,47 @@
+package club.codedemo.getuserinspringsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 新建测试用户  admin:admin
+     * @param auth 认证创建器
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .roles("")
+            .password(this.passwordEncoder().encode("admin"));
+    }
+
+    /**
+     * 任何地址均允许匿名允许以及启用httpBasic认证
+     * @param httpSecurity
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                    .antMatchers("/**")
+                    .permitAll()
+                    .and()
+                    .httpBasic();
+    }
+}
