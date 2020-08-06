6d69e926d746be0172b0e3d1516d404ff46e8e86
panjie <3792535@qq.com>
2020-08-06 14:15:20 +0800

    init

---
+++ src/main/java/club/codedemo/getuserinspringsecurity/GetUserInSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package club.codedemo.getuserinspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetUserInSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetUserInSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/codedemo/getuserinspringsecurity/GetUserInSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package club.codedemo.getuserinspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetUserInSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
